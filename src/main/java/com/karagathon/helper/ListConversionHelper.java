473ef7da4a0cc97783d2e26b186a9287782c2de5
jansenmarsonang@gmail.com <you@example.com>
2020-08-24 20:11:26 +0800

    Added missing classes

--- src/main/java/com/karagathon/helper/ListConversionHelper.java
+++ src/main/java/com/karagathon/helper/ListConversionHelper.java
@@ -1,10 +1,13 @@
 package com.karagathon.helper;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.karagathon.model.IModel;
 import com.karagathon.model.Media;
+import com.karagathon.model.Report;
 import com.karagathon.model.Violation;
 import com.karagathon.model.Violator;
 
@@ -14,12 +17,27 @@ public class ListConversionHelper {
 		return listOfString.stream().map(Long::parseLong).collect(Collectors.toList());
 	}
 	
-	public static List<Media> stringToMedia( Collection<String> listOfString, Violation violation ) {
-		return listOfString.stream().map( string -> new Media(string, violation) ).collect(Collectors.toList());
+	public static List<Media> stringToMedia(Collection<String> listOfString, IModel model) {
+		return listOfString.stream().map( string -> new Media(string, model) ).collect(Collectors.toList());
 	}
 	
-	public static List<Media> stringToMedia( Collection<String> listOfString, Violator violator ) {
-		return listOfString.stream().map( string -> new Media(string, violator) ).collect(Collectors.toList());
+	
+	@SuppressWarnings("unchecked")
+	public static List<String> objectToList(Object obj) {
+	    List<String> list = new ArrayList<>();
+	    if (obj instanceof Collection) {
+	        list = new ArrayList<>((Collection<String>)obj);
+	    }
+	    return list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<Object> objectToListObject(Object obj) {
+	    List<Object> list = new ArrayList<>();
+	    if (obj instanceof Collection) {
+	        list = new ArrayList<>((Collection<Object>)obj);
+	    }
+	    return list;
 	}
 	 
 }
