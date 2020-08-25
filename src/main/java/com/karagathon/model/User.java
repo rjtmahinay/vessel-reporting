d88d8a149bf4cf14511da8ba4dd851d10c543597
Tristan <rjt.mahinay@gmail.com>
2020-08-26 04:12:24 +0800

    feat: login and WebSecurity
    Signed-off-by: Tristan <rjt.mahinay@gmail.com>

--- src/main/java/com/karagathon/model/User.java
+++ src/main/java/com/karagathon/model/User.java
@@ -4,33 +4,58 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Entity
 @Table
 public class User {
-	
+
 	@Id
 	private String userId;
 	private String name;
 	private String password;
-	
+	private String role;
+	private boolean enabled;
 
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
-		this.password = password;
+		BCryptPasswordEncoder bEncoder = new BCryptPasswordEncoder();
+		this.password = bEncoder.encode(password);
 	}
-	
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
 }
